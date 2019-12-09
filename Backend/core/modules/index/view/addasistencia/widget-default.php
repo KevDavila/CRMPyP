72a518169b51d7c1fbe8fd73482f9aa87d461bbf
Kevin Davila <sabbathkvlt@gmail.com>
2019-12-09 13:08:20 -0600

    ultima actu

--- Backend/core/modules/index/view/addasistencia/widget-default.php
+++ Backend/core/modules/index/view/addasistencia/widget-default.php
@@ -11,7 +11,7 @@ if(count($_POST)>0){
 	$u = $user->add();
 
 
-//print "<script>window.location='index.php?view=assistance';</script>";
+print "<script>window.location='index.php?view=assistance';</script>";
 
 
 }
